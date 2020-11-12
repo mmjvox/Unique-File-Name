f903820a63a3feafad03416ef5c2a304392fe512
mmjvox <mmdjv.kh94@gmail.com>
2020-11-13 01:17:49 +0330

    add class file

---
+++ UniqueFileName.h
@@ -0,0 +1,35 @@
+#ifndef UNIQUEFILENAME_H
+#define UNIQUEFILENAME_H
+
+#include <QString>
+#include <QFileInfo>
+#include <QDir>
+
+class UniqueFileName
+{
+    public:
+    explicit UniqueFileName();
+    static inline QString newName(QString str)
+    {
+        int index=1;
+        QString fileName=str;
+        QFileInfo fileinfo;
+        fileinfo.setFile(fileName);
+        while(fileinfo.exists()){
+            QString completeName;
+            completeName=fileinfo.completeBaseName();
+            if(fileinfo.completeBaseName().endsWith( QString::number(index).prepend(" (").append(")") ))
+            {
+                completeName=completeName.left( completeName.lastIndexOf( QString::number(index).prepend(" (").append(")") ) );
+                index++;
+            }
+            fileName=fileinfo.path()
+                    .append(QDir::separator())
+                    .append( completeName )
+                    .append( QString::number(index).prepend(" (").append(")") )
+                    .append( fileinfo.suffix().prepend(".") );
+            fileinfo.setFile(fileName);
+        }
+        return fileName;
+    }
+};
